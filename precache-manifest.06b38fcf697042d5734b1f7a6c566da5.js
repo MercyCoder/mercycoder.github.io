8dad2e901f0b4d688194be9426210c73099dbf43
Mercy <mercyemeth@gmail.com>
2020-06-09 22:12:15 -0400

    Updates

---
+++ precache-manifest.06b38fcf697042d5734b1f7a6c566da5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9886ceee8a3622dfacf888b42e61e4aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90593da65412b1e47753",
+    "url": "/static/css/main.5ff149b4.chunk.css"
+  },
+  {
+    "revision": "928b871cef9e68e92256",
+    "url": "/static/js/2.35dae208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35dae208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90593da65412b1e47753",
+    "url": "/static/js/main.ddf8de76.chunk.js"
+  },
+  {
+    "revision": "786920618200c92cdd9b",
+    "url": "/static/js/runtime-main.921fd305.js"
+  },
+  {
+    "revision": "d9cc65b65f7c90e5987a83e255123820",
+    "url": "/static/media/player.d9cc65b6.jpg"
+  }
+]);
